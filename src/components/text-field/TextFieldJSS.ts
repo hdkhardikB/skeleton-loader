f7f204bc1404f5690bbba6cddb0481c9dfdd5d50
Hardik Babaria <hardik@everledger.io>
2020-01-23 20:16:05 +0530

    textboxes update

--- src/components/text-field/TextFieldJSS.ts
+++ src/components/text-field/TextFieldJSS.ts
@@ -2,6 +2,16 @@ import { Theme } from '@material-ui/core/styles/createMuiTheme';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import { EvlTheme } from './../../themes';
 
-const useStyles = makeStyles((theme: Theme) => createStyles({}), { name: 'evlTextBox', defaultTheme: EvlTheme });
+const useStyles = makeStyles(
+  (theme: Theme) =>
+    createStyles({
+      textField: {
+        background: theme.palette.primary.contrastText,
+        height: theme.spacing() * 5.2,
+        fontSize: `${(theme.spacing() * 1.6) / 20}rem`,
+      },
+    }),
+  { name: 'evlTextBox', defaultTheme: EvlTheme },
+);
 
 export default useStyles;
