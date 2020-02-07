698958fffca4ce85f864463f5268761fec27122f
Ajay Kumar <ajay@everledger.io>
2020-02-07 10:46:36 +0000

    push-docker-image-to-registry.sh edited online with Bitbucket

--- push-docker-image-to-registry.sh
+++ push-docker-image-to-registry.sh
@@ -7,6 +7,7 @@ export TAG=$1
 docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
 
 # Push latest to docker
+docker build -t evl-ui-components:latest .
 docker tag evl-ui-components:latest everledgerio/evl-ui-components:latest
 docker push everledgerio/evl-ui-components:latest
 
