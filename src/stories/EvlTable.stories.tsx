12ef7ce17631f6714592039841eb05f5558f7df3
Deval Chaudhari <deval@everledger.io>
2020-03-09 12:13:30 +0530

    Table Component added

---
+++ src/stories/EvlTable.stories.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { EvlTable } from '@components';
+
+const stories = storiesOf('EvlTable', module);
+
+function createData(assetId: string, color: string, weight: number, shape: String) {
+  return { assetId, color, weight, shape };
+}
+
+const rows = [
+  createData('ABC', 'red', 2, 'round'),
+  createData('Test2', 'green', 1, 'cushion'),
+  createData('Test3', 'black', 4, 'oval'),
+  createData('Test4', 'blue', 3, 'round'),
+  createData('Test5', 'white', 1, 'round'),
+  createData('Test6', 'grern', 2, 'round'),
+];
+
+const columns = [
+  {
+    name: 'assetId',
+    title: 'Asset Id',
+    options: {
+      sort: true,
+    },
+  },
+  {
+    name: 'color',
+    title: 'Color',
+    options: {
+      sort: true,
+    },
+  },
+  {
+    name: 'weight',
+    title: 'Weight',
+    options: {
+      sort: true,
+    },
+  },
+  {
+    name: 'shape',
+    title: 'Shape',
+  },
+];
+
+stories.add('Simple EvlTable', () => (
+  <EvlTable rows={rows} rowKey="assetId" columns={columns} onSelect={selectedIds => console.log(selectedIds)} />
+));
+
+stories.add('Selectable EvlTable', () => (
+  <EvlTable
+    rows={rows}
+    rowKey="assetId"
+    columns={columns}
+    selectable
+    onSelect={selectedIds => console.log(selectedIds)}
+  />
+));
--- templates/evl-ui-component-story/___Component___.stories.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import { storiesOf } from '@storybook/react';
-import { ___Component___ } from '@components';
-
-const stories = storiesOf("___Component___", module);
-
-stories.add("dummy", () => <___Component___ />);
