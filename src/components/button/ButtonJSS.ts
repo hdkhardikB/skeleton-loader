a95c98713dcd259e79e8da2a03a3a0b0603f86f7
div <divyesh@everledger.io>
2020-03-11 18:17:26 +0530

    # Button default design update

--- src/components/button/ButtonJSS.ts
+++ src/components/button/ButtonJSS.ts
@@ -1,17 +1,23 @@
 import { Theme } from '@material-ui/core/styles/createMuiTheme';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
-import { fade } from '@material-ui/core/styles/colorManipulator';
 import { EvlTheme } from '@themes';
 
 const useStyles = makeStyles(
   (theme: Theme) =>
     createStyles({
       root: {
-        background: fade(theme.palette.primary.main, 0.2),
-        cursor: 'unset',
+        cursor: 'pointer',
+        backgroundColor: theme.palette.primary.light,
+        borderColor: theme.palette.primary.light,
+        borderWidth: theme.spacing(0.1),
+        borderStyle: 'solid',
+        borderRadius: theme.shape.borderRadius * 5,
+        color: theme.palette.common.white,
+
         '&:hover': {
-          background: fade(theme.palette.primary.main, 0.2),
-        },
+          backgroundColor: 'transparent',
+          color: theme.palette.primary.light,
+        }
       },
     }),
   { name: 'evlButton', defaultTheme: EvlTheme },
