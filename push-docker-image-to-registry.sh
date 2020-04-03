3f8ede95cc930a8ceeef9eb43c7927a727fc0b78
Deval Chaudhari <deval@everledger.io>
2020-04-03 18:52:21 +0530

    docker file path updated

--- push-docker-image-to-registry.sh
+++ push-docker-image-to-registry.sh
@@ -4,7 +4,7 @@
 docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
 
 # Push latest to docker
-docker build -t evl-ui-components:latest
+docker build -t evl-ui-components:latest .
 docker tag evl-ui-components:latest everledgerio/evl-ui-components:latest
 docker push everledgerio/evl-ui-components:latest
 
