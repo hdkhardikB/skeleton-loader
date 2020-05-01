68863a186d95050ddcd407c2710cfd88740a9215
Hardik Babaria <hardik@Hardiks-MacBook-Pro.local>
2020-05-01 16:02:04 +0530

    new component for thumbnail

--- src/components/image-thumbnail/ImageThumbnailJSS.ts
+++ src/components/image-thumbnail/ImageThumbnailJSS.ts
@@ -5,7 +5,22 @@ import { EvlTheme } from '@themes';
 const useStyles = makeStyles(
   (theme: Theme) =>
     createStyles({
-      root: {},
+      figureImg: {
+        width: 'auto',
+        height: '100%',
+        padding: theme.spacing(0.3),
+        maxWidth: '100%',
+      }, figure: {
+        margin: theme.spacing(0),
+      },
+      figureBorder: {
+        width: theme.spacing(6.2),
+        height: theme.spacing(6.2),
+        borderWidth: theme.spacing(0.1),
+        borderStyle: 'solid',
+        borderColor: theme.palette.grey.A400,
+        borderRadius: theme.shape.borderRadius * 5,
+      },
     }),
   { name: 'evlImageThumbnail', defaultTheme: EvlTheme },
 );
