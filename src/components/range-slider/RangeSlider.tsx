b28c9486b5ddfbd878cceb3a38fa534158bbe9f6
hdkeverledger <hardik@everledger.io>
2020-07-16 23:46:36 +0530

    Textchange for slider control in filter

--- src/components/range-slider/RangeSlider.tsx
+++ src/components/range-slider/RangeSlider.tsx
@@ -1,23 +1,24 @@
 import * as React from 'react';
 import useStyles from './RangeSliderJSS';
-import {Slider, SliderProps } from '@material-ui/core';
+import { Slider, SliderProps, InputAdornment } from '@material-ui/core';
 import EvlTypography from '@components/typography';
 import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import EvlGrid from '@components/grid';
 import TextField from '@components/text-field';
 
-export interface EvlRangeSliderProps extends Omit<SliderProps, 'onChange'>  {
+export interface EvlRangeSliderProps extends Omit<SliderProps, 'onChange'> {
   label?: string;
   error?: string;
   valueText?: string;
-  onChange?: (event: React.ChangeEvent<{}>, newValue: number | number[]) => void;
+  onTextChange?: (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>, input: number) => void;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>, newValue: number | number[]) => void;
 }
 
-export const EvlRangeSlider: React.FC<EvlRangeSliderProps> = ({ value, label, valueText, error, ...props }) => {
+export const EvlRangeSlider: React.FC<EvlRangeSliderProps> = ({ value, onTextChange, label, valueText, error, ...props }) => {
   const classes = useStyles();
   const [minVaule, maxValue] = value as number[];
-  
+
   return (
     <FormControl className={classes.root} error={!!error}>
       {label && (
@@ -27,10 +28,14 @@ export const EvlRangeSlider: React.FC<EvlRangeSliderProps> = ({ value, label, va
       )}
       <EvlGrid container direction="row" justify="space-between">
         <EvlGrid item xs={4}>
-          <TextField value={`${minVaule} ${valueText}`}></TextField>
+          <TextField type="number" onChange={event => onTextChange && onTextChange(event, 0)} InputProps={{
+            endAdornment: <InputAdornment position="start">{valueText}</InputAdornment>,
+          }} value={minVaule}></TextField>
         </EvlGrid>
         <EvlGrid item xs={4}>
-          <TextField value={`${maxValue} ${valueText}`}></TextField>
+          <TextField type="number" onChange={event => onTextChange && onTextChange(event, 1)} InputProps={{
+            endAdornment: <InputAdornment position="start">{valueText}</InputAdornment>,
+          }} value={maxValue}></TextField>
         </EvlGrid>
       </EvlGrid>
       <Slider value={value} {...props} />
